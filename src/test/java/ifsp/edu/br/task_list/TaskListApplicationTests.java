277b46c665930cc2881a41dc61187f658263b3c3
carlos <carloslimacdn@gmail.com>
2024-12-06 08:37:57 -0300

    first commit

---
+++ src/test/java/ifsp/edu/br/task_list/TaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsp.edu.br.task_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
