ce5fe3c960efb9cb3ddb5c41d143601033519546
carlos <carloslimacdn@gmail.com>
2024-12-15 12:07:48 -0300

    modified:   src/main/java/ifsp/edu/br/task_list/controller/LoginController.java 	modified:   src/main/resources/application.properties 	modified:   src/main/resources/static/css/styles.css 	modified:   src/main/resources/templates/login.html

--- src/main/java/ifsp/edu/br/task_list/controller/LoginController.java
+++ src/main/java/ifsp/edu/br/task_list/controller/LoginController.java
@@ -4,6 +4,7 @@ import ifsp.edu.br.task_list.model.Usuario;
 import ifsp.edu.br.task_list.service.UsuarioService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -18,21 +19,22 @@ public class LoginController {
     }
 
     @GetMapping("/login")
-    public String loginPage() {
+    public String loginPage(@ModelAttribute("error") String error) {
+        System.out.println("Erro recebido: " + error);
         return "login";
     }
 
     @PostMapping("/login")
     public String login(@RequestParam("email") String email,
-            @RequestParam("password") String password,
-            RedirectAttributes redirectAttributes) {
+            @RequestParam("password") String password) {
         Usuario usuario = usuarioService.findByEmail(email);
 
         if (usuario != null && usuarioService.validarSenha(usuario, password)) {
             return "redirect:/projetos";
         }
 
-        redirectAttributes.addFlashAttribute("error", "Email ou senha inválidos");
-        return "redirect:/login";
+        // Passa o erro na query string
+        return "redirect:/login?error=true";
     }
+
 }
